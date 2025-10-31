e9412aee22fe6ad62b7fb660d6493fbfb80418ac
Rodrigo Zanetta Slaski Suchorzewski <rodrigo.suchorzewski@telefonica.com>
2025-10-31 19:10:59 -0300

    Adicionado a feature GetBasket no Basket.

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,5 +1,20 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketEndpoint
+public class GetBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{username}", async (string username, IMediator mediator) =>
+        {
+            var result = await mediator.SendQueryAsync<GetBasketQuery, GetBasketResult>(new GetBasketQuery(username));
+            var response = result.Adapt<GetBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
 }
---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketResponse(ShoppingCart Cart);
